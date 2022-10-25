cabab4897007722c76036f22985ea35df58fdee4
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2022-10-25 15:06:29 +0200

    zad3 zrobione

--- src/Race.java
+++ src/Race.java
@@ -23,8 +23,20 @@ public class Race {
                 switch (option) {
                     case 1:
                         for (int i = 0; i < names.length; i++) {
-                            /* Write your code here */
+                            for (int j = 0; j < (times.length - (i + 1)); j++){
+                                if (times[j] > times[j + 1]){
+                                    int temp = times[j];
+                                    times[j] = times[j + 1];
+                                    times[j + 1] = temp;
+                                    String tmp = names[j];
+                                    names[j] = names[j + 1];
+                                    names[j + 1] = tmp;
+                                }
+                            }
                         }
+                        System.out.println("1 miejsce : " + names[names.length-1] + "   " + times[times.length-1]);
+                        System.out.println("2 miejsce : " + names[names.length-2] + "   " + times[times.length-2]);
+                        System.out.println("3 miejsce : " + names[names.length-3] + "   " + times[times.length-3]);
                         break;
                     case 2:
                         return;
